3d5646d5e6ea341eaee429009411a5a4f79dd6e8
Pedro Estrada <p89dominguez@gmail.com>
2021-07-26 21:45:33 -0300

    fix

--- validations/crear-valid.php
+++ validations/crear-valid.php
@@ -11,14 +11,14 @@ $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $password_conf = $_POST['password_conf'];
 
-if($id=== 0){
+if($id == 0){
     $query =  "INSERT INTO `usuario` (`id`, `nombre`, `mail`, `telefono`, `username`, `password`) VALUES (null,'".$nombre."','".$mail."','$phone','$username','$password')";
 }
 else{
     //update
     $query = "UPDATE `usuario` SET `username` = '$username',`nombre` = '$nombre',`telefono` = '$phone',`mail` = '$mail' WHERE `usuario`.`id` = $id";
 }
-    
+
 $connection->query($query);
 print_r($connection->insert_id);
 
