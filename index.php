092496021bb4681e4f25d3a49c9f483a1271e6f9
Pedro Estrada <p89dominguez@gmail.com>
2021-08-23 20:53:08 -0300

    filter

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ include('shared/header.php');
 $user = isset($_SESSION['username'])?$_SESSION['username']:"";
 
 $my_Usr = "Username: ".  $user;
+$termino = "";
+if(isset($_GET['termino']))
+{
+  $termino = $_GET['termino'];
+}
+include('db.php'); 
+$q = "SELECT id,nombre,mail,telefono,username,imagen FROM usuario";
+$response = $connection->query($q);
+
 
 ?>
 
@@ -12,6 +21,18 @@ $my_Usr = "Username: ".  $user;
   <!-- Flexbox container for aligning the toasts -->
     <a href="crear.php" class="btn btn-primary">Crear</a>
     <a id="test" class="btn btn-primary">Sweet</a>
+    <br>
+    <div class="row">
+      <div class="col-4" >
+        <br>
+      <form action="index.php" method="GET" style="display: flex;flex-direction:row">
+        <input type="text" name="termino" value="<?php echo isset($_GET['termino'])? $_GET['termino'] : "" ?>" class="form-control" placeholder="Filtrar">
+        <button type="submit" href="index.php" class="btn btn-danger">Filtrar</button>
+    </form>
+      </div>
+    </div>
+
+    
   <table class="table">
   <thead>
     <tr>
@@ -25,13 +46,17 @@ $my_Usr = "Username: ".  $user;
   </thead>
   <tbody>
   <?php
-    include('db.php'); 
-    $q = "SELECT id,nombre,mail,telefono,username,imagen FROM usuario";
-    $response = $connection->query($q);
 
     if ($response->num_rows > 0) {
         while($row = $response->fetch_assoc()) {
-            echo '<tr id="'.$row["id"].'"><td>'.$row["nombre"].'</td><td>'.$row["mail"].'</td><td>'.$row["telefono"].'</td><td>'.$row["username"].'</td><td><img style="width:50px;" src="upload/'.$row["imagen"].'"></td><td style="display:flex"><form action="crear.php" method="get"><input type="hidden" value="'.$row["id"].'" name="id"><button type="submit" class="btn btn-primary">Editar</button></form><form id="deleteForm'.$row["id"].'" action="validations/confirm-delete.php" method="post"><input name="id" type="hidden" value="'.$row["id"].'"><button type="submit" id="delete'.$row["id"].'" onclick="clickHandler(event)" class="btn btn-danger">Eliminar</button></form></td> </tr>';              
+          if($termino != ""){
+            if(strpos(strtolower($row["nombre"]),strtolower($termino)) !== false || strpos(strtolower($row["telefono"]),strtolower($termino)) !== false){
+              echo '<tr id="'.$row["id"].'"><td>'.$row["nombre"].'</td><td>'.$row["mail"].'</td><td>'.$row["telefono"].'</td><td>'.$row["username"].'</td><td><img style="width:50px;" src="upload/'.$row["imagen"].'"></td><td style="display:flex"><form action="crear.php" method="get"><input type="hidden" value="'.$row["id"].'" name="id"><button type="submit" class="btn btn-primary">Editar</button></form><form id="deleteForm'.$row["id"].'" action="validations/confirm-delete.php" method="post"><input name="id" type="hidden" value="'.$row["id"].'"><button type="submit" id="delete'.$row["id"].'" onclick="clickHandler(event)" class="btn btn-danger">Eliminar</button></form></td> </tr>';      
+            }
+          }
+          else{
+            echo '<tr id="'.$row["id"].'"><td>'.$row["nombre"].'</td><td>'.$row["mail"].'</td><td>'.$row["telefono"].'</td><td>'.$row["username"].'</td><td><img style="width:50px;" src="upload/'.$row["imagen"].'"></td><td style="display:flex"><form action="crear.php" method="get"><input type="hidden" value="'.$row["id"].'" name="id"><button type="submit" class="btn btn-primary">Editar</button></form><form id="deleteForm'.$row["id"].'" action="validations/confirm-delete.php" method="post"><input name="id" type="hidden" value="'.$row["id"].'"><button type="submit" id="delete'.$row["id"].'" onclick="clickHandler(event)" class="btn btn-danger">Eliminar</button></form></td> </tr>';      
+          }
         }
     } else {
         printf('No record found.<br />');
